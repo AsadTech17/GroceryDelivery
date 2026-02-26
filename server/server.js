f0f4178cb4130f5efd9ada6fc5859381825528eb
Ali Asad <aliasad0443@gmail.com>
2026-02-27 00:16:10 +0500

    add user model and controller

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import cors from "cors";
-// import connectDB from "./configs/db.js";
-// import "dotenv/config";
+import connectDB from "./config/db.js";
+import "dotenv/config";
 // import userRouter from "./routes/userRoute.js";
 // import connectCloudinary from "./configs/cloudinary.js";
 // import sellerRouter from "./routes/sellerRoute.js";
@@ -14,21 +14,20 @@ import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 4000;
-// await connectDB();
+await connectDB();
 // await connectCloudinary();
 //Allow multiple origins
 
-// const allowedOrigins = [
-//   "http://localhost:5173",
-//   "https://greatstack-frontend-iota.vercel.app",
-// ];
+const allowedOrigins = [
+  "http://localhost:5173",
+];
 
 // app.post("/stripe", express.raw({ type: "application/json" }), stripeWebhooks);
 
 //Middleware configuration
-// app.use(express.json());
-// app.use(cookieParser());
-// app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 app.get("/", (req, res) => res.send("API is Working"));
 // app.use("/api/user", userRouter);
