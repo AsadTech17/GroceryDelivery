9d6f17615fb6ff3a2c9486de33ae1e9035f1d4b4
Ali Asad <aliasad0443@gmail.com>
2026-02-28 23:10:45 +0500

    sellerController

--- server/controllers/sellerController.js
+++ server/controllers/sellerController.js
@@ -29,26 +29,26 @@ export const sellerLogin = async (req, res) => {
 };
 
 //Seller isAuth :/api/seller/is-auth
-// export const isSellerAuth = async (req, res) => {
-//   try {
-//     return res.json({ success: true });
-//   } catch (error) {
-//     console.log(error.message);
-//     res.json({ success: false, message: error.message });
-//   }
-// };
+export const isSellerAuth = async (req, res) => {
+  try {
+    return res.json({ success: true });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 //Logout Seller:/api/seller/logout
-// export const SellerLogout = async (req, res) => {
-//   try {
-//     res.clearCookie("sellerToken", {
-//       httpOnly: true,
-//       secure: process.env.NODE_ENV === "production",
-//       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-//     });
-//     return res.json({ success: true, message: "Logged Out" });
-//   } catch (error) {
-//     console.log(error.message);
-//     res.json({ success: false, message: error.message });
-//   }
-// };
+export const SellerLogout = async (req, res) => {
+  try {
+    res.clearCookie("sellerToken", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    });
+    return res.json({ success: true, message: "Logged Out" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
