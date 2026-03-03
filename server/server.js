d0c5e38df31ba83f2cfad6ae54bc656f06175cd7
Ali Asad <aliasad0443@gmail.com>
2026-03-03 23:21:42 +0500

    add new routes and controllers

--- server/server.js
+++ server/server.js
@@ -4,11 +4,17 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 import "dotenv/config";
 import userRouter from "./routes/userRoutes.js";
+import sellerRouter from "./routes/sellerRoutes.js";
+import connectCloudinary from "./config/cloudinary.js";
+import productRouter from "./routes/productRoutes.js";
+import cartRouter from "./routes/cartRoute.js";
+import addressRouter from "./routes/addressRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
 await connectDB();
-// await connectCloudinary();
+await connectCloudinary();
 
 //Allow multiple origins
 const allowedOrigins = [
@@ -24,11 +30,11 @@ app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 app.get("/", (req, res) => res.send("API is Working"));
 app.use("/api/user", userRouter);
-// app.use("/api/seller", sellerRouter);
-// app.use("/api/product", productRouter);
-// app.use("/api/cart", cartRouter);
-// app.use("/api/address", addressRouter);
-// app.use("/api/order", orderRouter);
+app.use("/api/seller", sellerRouter);
+app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/address", addressRouter);
+app.use("/api/order", orderRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
